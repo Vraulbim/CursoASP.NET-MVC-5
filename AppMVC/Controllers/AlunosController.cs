ae8deaf4d638cc01fcf7d55515534ac63e7e0e0a
Vitor Raul Bim <vitorrb36@gmail.com>
2021-09-15 08:59:14 -0300

    Criação do repositório

---
+++ AppMVC/Controllers/AlunosController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AppMVC.Models;
+
+namespace AppMVC.Controllers
+{
+    public class AlunosController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [HttpGet]
+        [Route("listar-alunos")]
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Alunos.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("aluno-detalhe/{id:int}")]
+        public async Task<ActionResult> Details(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var aluno = await db.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        [HttpGet]
+        [Route("novo-aluno")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("novo-aluno")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,name,email,cpf,data_matricula,ativo")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Alunos.Add(aluno);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(aluno);
+        }
+
+        [HttpGet]
+        [Route("editar-aluno/{id:int}")]
+        public async Task<ActionResult> Edit(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aluno aluno = await db.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        [HttpPost]
+        [Route("editar-aluno/{id:int}")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,name,email,cpf,data_matricula,ativo")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aluno).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(aluno);
+        }
+
+        [HttpGet]
+        [Route("excluir-aluno/{id:int}")]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aluno aluno = await db.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        [HttpPost]
+        [Route("excluir-aluno/{id:int}")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Aluno aluno = await db.Alunos.FindAsync(id);
+            db.Alunos.Remove(aluno);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AppMVC/Models/Aluno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AppMVC.Models
+{
+    public class Aluno
+    {
+        [Key]
+        public int id { get; set; }
+
+        [DisplayName("Nome Completo")]
+        [Required(ErrorMessage ="{0} é um campo obrigatório")]
+        [MaxLength(100, ErrorMessage ="Número máximo de caracteres atingido(100)")]
+        public string name { get; set; }
+
+        [DisplayName("E-mail")]
+        [Required(ErrorMessage ="É necessário informar um {0}")]
+        [EmailAddress(ErrorMessage ="E-mail inválido, verifique e tente novamente")]
+        public string email { get; set; }
+
+        [DisplayName("CPF")]
+        [Required(ErrorMessage ="{0} não pode ser nulo")]
+        [MaxLength(11, ErrorMessage ="{0} deve conter 11 caracteres")]
+        public string cpf { get; set; }
+
+        public DateTime data_matricula { get; set; }
+
+        public bool ativo { get; set; }
+    }
+}
